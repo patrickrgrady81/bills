4a6dc9d05eb3c9173e5d40dd1fb331c8cfedab57
Patrick Grady <patrickrgrady81@gmail.com>
2022-06-27 08:10:36 -0400

    Added Button and Modal for new bills

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import { useState } from 'react'; 
+import { Button, Modal, Form } from 'react-bootstrap';
+
 function App() {
+  
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  
+  function handleModal() {
+    setIsModalOpen(!isModalOpen);
+  }
+
+  function handleNewBill(e) {
+    e.preventDefault();
+    alert("Create New Bill");
+    setIsModalOpen(false);
+  }
+
   return (
     <>
-      Hello World!
+      <Button onClick={() => handleModal()}>New Bill</Button>
+
+      { isModalOpen &&
+      (<Modal.Dialog>
+        <Modal.Header closeButton onHide={() => handleModal()}>
+          <Modal.Title>Add Bill</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <Form>
+
+          </Form>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => handleModal()}>Close</Button>
+          <Button variant="primary" type="submit" onClick={(e) => handleNewBill(e)}>Save changes</Button>
+        </Modal.Footer>
+      </Modal.Dialog>  )}
     </>
   );
 }
