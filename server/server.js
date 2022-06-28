5a37e0d5548abd30b77e482a8871cfa9fdc6e442
Patrick Grady <patrickrgrady81@gmail.com>
2022-06-28 06:34:24 -0400

    connect to db

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const port = 3001;
+
+mongoose.connect(process.env.DATABASE_URL, () => {console.log("Connected to Database")});
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.json({ message: "Hey Man" });
   })
 
-app.listen(port, () => console.log(`Server Started on Port ${port}`));
+app.listen(parseInt(process.env.PORT), () => console.log(`Server Started on Port ${parseInt(process.env.PORT)}`));
